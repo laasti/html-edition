b086107cce8a7392efb8746b7c266ba4098232e0
nebulousGirl <contact@nebulousweb.com>
2016-04-20 22:27:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Laasti/html-edition
+
+## Installation
+
+```
+composer require laasti/html-edition
+```
+
+## Usage
+
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+See Git commits and Github's releases
+
+## Credits
+
+Author: Sonia Marquette (@nebulousGirl)
+
+## License
+
+Released under the MIT License. See LICENSE.txt file.
---
+++ src/ServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Laasti\HtmlEdition;
+
+class ServiceProvider extends \League\Container\ServiceProvider\AbstractServiceProvider implements \League\Container\ServiceProvider\BootableServiceProviderInterface
+{
+
+    public function register()
+    {
+        //Do nothing
+    }
+    
+    public function boot()
+    {
+        $container = $this->getContainer();
+        $container->addServiceProvider('Laasti\Core\Providers\ConfigFilesProvider');
+        $container->addServiceProvider('Laasti\Directions\Providers\LeagueDirectionsProvider');
+        $container->addServiceProvider('Laasti\Peels\Providers\LeaguePeelsProvider');
+        $container->addServiceProvider('Laasti\Log\MonologProvider');
+        $container->addServiceProvider('Laasti\Lazydata\Providers\LeagueLazydataProvider');
+        $container->addServiceProvider('Laasti\Views\Providers\LeagueViewsProvider');
+        $container->addServiceProvider('Laasti\SymfonyTranslationProvider\SymfonyTranslationProvider');
+
+    }
+    
+}
